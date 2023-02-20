cc62646f1bd0599d3090ec1e5d928f062b5aa2bf
hugocode95 <123723780+hugocode95@users.noreply.github.com>
2023-02-20 21:28:27 +0100

    Stucture index.html, js et debut du CSS pour le menu

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+let menuSelector = document.getElementsByClassName('navCross');
+let content = document.getElementsByClassName('header');
+
+console.log(content[0].style);
+
+menuSelector[0].addEventListener('change', () => {
+    if (menuSelector[0].checked) {
+        console.log(menuSelector[0].checked);
+        content[0].style.height = "0px";
+        console.log(content[0].style);
+    } else {
+        console.log(menuSelector[0].checked);
+        content[0].style.height = "100vh";
+        console.log(content[0].style);
+    }
+})
